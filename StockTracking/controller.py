8122b9a9714c6717e3b8da7875e21cd332ab932f
Doraon <ssssunny2016@gmail.com>
2018-04-24 14:46:12 -0400

    Rewrite db initialization

--- StockTracking/controller.py
+++ StockTracking/controller.py
@@ -1,8 +1,8 @@
 # init
 from flask import Flask, render_template, request, redirect, url_for
-#from StockTracking.backendserver.rss import rss
+from StockTracking.backendserver.rss import rss
 from flask import request, render_template, jsonify
-# from StockTracking.backendserver.data import read_file
+from StockTracking.backendserver.data import read_file
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -38,8 +38,6 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = StringField('password', validators=[InputRequired(), Length(min=8, max=80)])
@@ -111,93 +109,16 @@ def index():
 def get_news():
     ticker = request.form.get('ticker')
     print('get news about ' + ticker)
-    return jsonify(feed(ticker))
+    return jsonify(rss.feed(ticker))
 
 
 @app.route('/backend/get_price', methods=['GET', 'POST'])
 def get_price():
     ticker = request.form.get('ticker')
     print('get price about ' + ticker)
-    return jsonify(getData(ticker))
+    return jsonify(read_file.getData(ticker))
 
 
 @app.route('/backend/query_info', methods=['GET', 'POST'])
 def query_info():
     pass
-
-
-def feed(ticker: str):
-    rss_url = 'http://finance.yahoo.com/rss/headline?s=' + ticker
-    # get rss
-    feeds = feedparser.parse(rss_url)
-    rss = dict()
-    # get version of rss
-    # print(feeds.version)
-
-    # get http head
-    # print(feeds.headers)
-    # print(feeds.headers.get('Content-Type'))
-
-    # rss title
-    rss['title'] = feeds['feed']['title']
-    # rss link
-    rss['link'] = feeds['feed']['link']
-    # rss subtitle
-    rss['sub title'] = feeds['feed']['subtitle']
-    # number of articles
-    print(feeds)
-    n = len(feeds['entries'])
-    rss['number'] = n
-    rss['article'] = [dict() for i in range(n)]
-
-    for i in range(n):
-        rss['article'][i]['index'] = i
-        rss['article'][i]['title'] = feeds['entries'][i]['title']
-        rss['article'][i]['link'] = feeds['entries'][i]['link']
-        rss['article'][i]['date'] = feeds['entries'][i]['published_parsed']
-        rss['article'][i]['summary'] = feeds['entries'][i]['summary']
-
-    return rss
-
-
-def getData(sys):
-    dirname = os.path.dirname(__file__)
-    path = '/CSV/'+sys+'.csv'
-    # print(dirname)
-    f = open(dirname+path, 'r')
-    csv_f = csv.reader(f)
-    date = []
-    Open = []
-    High = []
-    Low = []
-    Close = []
-    for row in csv_f:
-        dateItem = row[0]
-        openItem = float(row[1])
-        highItewm = float(row[2])
-        lowItem = float(row[3])
-        closeItem = float(row[4])
-        date.append(dateItem)
-        Open.append(openItem)
-        High.append(highItewm)
-        Low.append(lowItem)
-        Close.append(closeItem)
-    # print data
-    f.close()
-
-    # define the json format
-    data = dict()
-    data['date'] = date
-    data['open'] = Open
-    data['high'] = High
-    data['low'] = Low
-    data['close'] = Close
-    return data
-
-
-# if __name__ == '__main__':
-#     print(getData("AMZN_historical"))
-#
-#
-# if __name__ == '__main__':
-#     print(feed('AMZN'))
